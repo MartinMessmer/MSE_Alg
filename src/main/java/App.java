2b2fa93006b8486e875eef54a8fdf943c277e2e9
Robert Rajakone <robie@gmail.com>
2017-12-07 15:57:24 +0100

    init

---
+++ src/main/java/App.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        loadFile();
+    }
+
+    private static void loadFile() {
+        URI path = null;
+        try {
+            path = Thread.currentThread().getContextClassLoader().getResource("gitfs.csv").toURI();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        List<String> lines = new LinkedList<>();
+        try {
+            lines = Files.readAllLines(Paths.get(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+       System.out.println(lines.size());
+    }
+}
