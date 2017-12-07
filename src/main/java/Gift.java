840cf6a67dc065a322e92494b036f5aaf8ab9efa
Martin Messmer <messmermartin@hotmail.com>
2017-12-07 16:20:37 +0100

    Add Gift Class

---
+++ src/main/java/Gift.java
@@ -0,0 +1,38 @@
+public class Gift {
+    private int id;
+    private double latitude;
+    private double longitude;
+    private int weight;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
